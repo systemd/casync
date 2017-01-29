6990023052e0e08cc7bc8064057ef9048511fdcf
Lennart Poettering <lennart@poettering.net>
2017-01-29 18:49:52 +0100

    tests: use random paths

--- test/test-casync.c
+++ test/test-casync.c
@@ -5,10 +5,15 @@
 #include "util.h"
 
 int main(int argc, char *argv[]) {
+        char *teststore, *testindex, *testtree;
         CaSync *s;
         int r, base_fd;
         CaChunkID digest;
         char t[CA_CHUNK_ID_FORMAT_MAX];
+
+        assert_se(asprintf(&teststore, "/var/tmp/teststore.%" PRIx64, random_u64()) >= 0);
+        assert_se(asprintf(&testindex, "/var/tmp/testindex.%" PRIx64, random_u64()) >= 0);
+        assert_se(asprintf(&testtree, "/var/tmp/testtree.%" PRIx64, random_u64()) >= 0);
 
         assert_se(s = ca_sync_new_encode());
 
@@ -16,8 +21,8 @@ int main(int argc, char *argv[]) {
         assert_se(base_fd >= 0);
         assert_se(ca_sync_set_base_fd(s, base_fd) >= 0);
 
-        assert_se(ca_sync_set_store_path(s, "/var/tmp/teststore") >= 0);
-        assert_se(ca_sync_set_index_path(s, "/var/tmp/testindex") >= 0);
+        assert_se(ca_sync_set_store_path(s, teststore) >= 0);
+        assert_se(ca_sync_set_index_path(s, testindex) >= 0);
 
         for (;;) {
                 r = ca_sync_step(s);
@@ -41,14 +46,14 @@ step2:
         ca_sync_unref(s);
         assert_se(s = ca_sync_new_decode());
 
-        (void) mkdir("/var/tmp/testtree", 0777);
-        base_fd = open("/var/tmp/testtree", O_RDONLY|O_CLOEXEC|O_DIRECTORY);
+        (void) mkdir(testtree, 0777);
+        base_fd = open(testtree, O_RDONLY|O_CLOEXEC|O_DIRECTORY);
 
         assert_se(base_fd >= 0);
         assert_se(ca_sync_set_base_fd(s, base_fd) >= 0);
 
-        assert_se(ca_sync_set_store_path(s, "/var/tmp/teststore") >= 0);
-        assert_se(ca_sync_set_index_path(s, "/var/tmp/testindex") >= 0);
+        assert_se(ca_sync_set_store_path(s, teststore) >= 0);
+        assert_se(ca_sync_set_index_path(s, testindex) >= 0);
 
         for (;;) {
                 r = ca_sync_step(s);
