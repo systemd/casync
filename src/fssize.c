4539285ba7cf883deb3e657a00f1d3fecdc092bf
Lennart Poettering <lennart@poettering.net>
2017-02-23 18:36:45 +0100

    fssize: add support for reading the size of ext2/ext3/ext4 superblocks

--- src/fssize.c
+++ src/fssize.c
@@ -6,9 +6,10 @@
 
 int read_file_system_size(int fd, uint64_t *ret) {
 
+        /* The squashfs and Android bootimg super block starts at offset 1024 */
         union {
                 struct {
-                        le32_t magic;
+                        le32_t s_magic;
                         le32_t inodes;
                         le32_t mkfs_time;
                         le32_t block_size;
@@ -45,38 +46,79 @@ int read_file_system_size(int fd, uint64_t *ret) {
 
                         /* ignore the rest */
                 } _packed_ android_bootimg;
-        } superblock;
+        } superblock0;
+
+        /* The ext2, ext3, ext4 superblock starts at offset 1024 */
+        union {
+                struct {
+                        le32_t s_inodes_count;
+                        le32_t s_blocks_count;
+                        le32_t s_r_blocks_count;
+                        le32_t s_free_blocks_count;
+                        le32_t s_free_inodes_count;
+                        le32_t s_first_data_block;
+                        le32_t s_log_block_size;
+                        le32_t s_log_frag_size;
+                        le32_t s_blocks_per_group;
+                        le32_t s_frags_per_group;
+                        le32_t s_inodes_per_group;
+                        le32_t s_mtime;
+                        le32_t s_wtime;
+                        le16_t s_mnt_count;
+                        le16_t s_max_mnt_count;
+                        le16_t s_magic;
+
+                        /* ignore the rest */
+                } _packed_ ext234;
+        } superblock1024;
 
         ssize_t n;
 
         assert(fd >= 0);
         assert(ret);
 
-        n = pread(fd, &superblock, sizeof(superblock), 0);
+        n = pread(fd, &superblock0, sizeof(superblock0), 0);
         if (n < 0)
                 return -errno;
-        if (n != sizeof(superblock))
-                return 0; /* don't know such short superblocks */
+        if (n != sizeof(superblock0))
+                return 0; /* don't know such short superblock0s */
 
-        if (le32toh(superblock.squashfs.magic == SQUASHFS_MAGIC)) {
-                *ret = ALIGN_TO(le64toh(superblock.squashfs.bytes_used), UINT64_C(4096));
+        if (le32toh(superblock0.squashfs.s_magic == SQUASHFS_MAGIC)) {
+                *ret = ALIGN_TO(le64toh(superblock0.squashfs.bytes_used), UINT64_C(4096));
                 return 1;
         }
 
-        if (le32toh(superblock.android_bootimg.magic) == _ANDROID_BOOTIMG_MAGIC_1 &&
-            le32toh(superblock.android_bootimg.magic2) == _ANDROID_BOOTIMG_MAGIC_2) {
+        if (le32toh(superblock0.android_bootimg.magic) == _ANDROID_BOOTIMG_MAGIC_1 &&
+            le32toh(superblock0.android_bootimg.magic2) == _ANDROID_BOOTIMG_MAGIC_2) {
                 uint32_t pagesize;
 
-                pagesize = le32toh(superblock.android_bootimg.page_size);
+                pagesize = le32toh(superblock0.android_bootimg.page_size);
                 if (__builtin_popcount(pagesize) != 1)
                         return -EBADMSG;
 
                 *ret = ALIGN_TO(608, pagesize) /* header size */ +
-                        ALIGN_TO(le32toh(superblock.android_bootimg.kernel_size), pagesize) +
-                        ALIGN_TO(le32toh(superblock.android_bootimg.initrd_size), pagesize) +
-                        ALIGN_TO(le32toh(superblock.android_bootimg.second_size), pagesize) +
-                        ALIGN_TO(le32toh(superblock.android_bootimg.dtb_size), pagesize);
+                        ALIGN_TO(le32toh(superblock0.android_bootimg.kernel_size), pagesize) +
+                        ALIGN_TO(le32toh(superblock0.android_bootimg.initrd_size), pagesize) +
+                        ALIGN_TO(le32toh(superblock0.android_bootimg.second_size), pagesize) +
+                        ALIGN_TO(le32toh(superblock0.android_bootimg.dtb_size), pagesize);
 
+                return 1;
+        }
+
+        n = pread(fd, &superblock1024, sizeof(superblock1024), 1024);
+        if (n < 0)
+                return -errno;
+        if (n != sizeof(superblock1024))
+                return 0; /* don't know this */
+
+        if (le16toh(superblock1024.ext234.s_magic) == EXT2_SUPER_MAGIC) {
+                uint64_t shift;
+
+                shift = 10 + le32toh(superblock1024.ext234.s_log_block_size);
+                if (shift >= 64)
+                        return -EBADMSG;
+
+                *ret = le32toh(superblock1024.ext234.s_blocks_count) * (UINT64_C(1) << shift);
                 return 1;
         }
 
