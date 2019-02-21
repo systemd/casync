80d8204629c28ce89ec472d7e069e81fb45a8106
Lennart Poettering <lennart@poettering.net>
2019-02-21 15:53:53 +0100

    editor: update formatting rules from systemd

--- .dir-locals.el
+++ .dir-locals.el
@@ -5,10 +5,7 @@
 ; NOTE: If you update this file make sure to update .vimrc and .editorconfig,
 ; too.
 
-((nil . ((indent-tabs-mode . nil)
-         (tab-width . 8)
-         (fill-column . 79)))
- (c-mode . ((fill-column . 119)
+((c-mode . ((fill-column . 109)
             (c-basic-offset . 8)
             (eval . (c-set-offset 'substatement-open 0))
             (eval . (c-set-offset 'statement-case-open 0))
@@ -16,5 +13,12 @@
             (eval . (c-set-offset 'arglist-intro '++))
             (eval . (c-set-offset 'arglist-close 0))))
  (nxml-mode . ((nxml-child-indent . 2)
-               (fill-column . 119)))
- (meson-mode . ((meson-indent-basic . 8))))
+               (fill-column . 109)))
+ (meson-mode . ((meson-indent-basic . 8)))
+ (sh-mode . ((sh-basic-offset . 8)
+             (sh-indentation . 8)))
+ (awk-mode . ((c-basic-offset . 8)))
+ (nil . ((indent-tabs-mode . nil)
+         (tab-width . 8)
+         (fill-column . 79)))
+)
