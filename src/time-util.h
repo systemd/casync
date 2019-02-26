cdbfbf9855a2c65be93b7c7f40c262b95e155680
Arnaud Rebillout <arnaud.rebillout@collabora.com>
2019-02-26 17:38:23 +0700

    format_timespan: use UINT64_C() macro
    Once again for consistency with the rest of casync's source
    
    Signed-off-by: Arnaud Rebillout <arnaud.rebillout@collabora.com>

--- src/time-util.h
+++ src/time-util.h
@@ -5,16 +5,16 @@
 
 #define NSEC_INFINITY ((uint64_t) -1)
 
-//#define NSEC_PER_SEC  ((uint64_t) 1000000000ULL) already defined in util.h
-#define NSEC_PER_MSEC ((uint64_t) 1000000ULL)
-#define NSEC_PER_USEC ((uint64_t) 1000ULL)
+//#define NSEC_PER_SEC  ((uint64_t) UINT64_C(1000000000)) already defined in util.h
+#define NSEC_PER_MSEC ((uint64_t) UINT64_C(1000000))
+#define NSEC_PER_USEC ((uint64_t) UINT64_C(1000))
 
-#define NSEC_PER_MINUTE ((uint64_t) (60ULL*NSEC_PER_SEC))
-#define NSEC_PER_HOUR ((uint64_t) (60ULL*NSEC_PER_MINUTE))
-#define NSEC_PER_DAY ((uint64_t) (24ULL*NSEC_PER_HOUR))
-#define NSEC_PER_WEEK ((uint64_t) (7ULL*NSEC_PER_DAY))
-#define NSEC_PER_MONTH ((uint64_t) (2629800ULL*NSEC_PER_SEC))
-#define NSEC_PER_YEAR ((uint64_t) (31557600ULL*NSEC_PER_SEC))
+#define NSEC_PER_MINUTE ((uint64_t) (UINT64_C(60)*NSEC_PER_SEC))
+#define NSEC_PER_HOUR ((uint64_t) (UINT64_C(60)*NSEC_PER_MINUTE))
+#define NSEC_PER_DAY ((uint64_t) (UINT64_C(24)*NSEC_PER_HOUR))
+#define NSEC_PER_WEEK ((uint64_t) (UINT64_C(7)*NSEC_PER_DAY))
+#define NSEC_PER_MONTH ((uint64_t) (UINT64_C(2629800)*NSEC_PER_SEC))
+#define NSEC_PER_YEAR ((uint64_t) (UINT64_C(31557600)*NSEC_PER_SEC))
 
 char *format_timespan(char *buf, size_t l, uint64_t t, uint64_t accuracy);
 
